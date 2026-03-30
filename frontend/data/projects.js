afb20e1b9b6f5bafc7174c148ab916004f75ad7a
Aziz Rahmad <rahmadaziz288@gmail.com>
2026-03-31 03:36:00 +0700

    add project

--- frontend/data/projects.js
+++ frontend/data/projects.js
@@ -1,5 +1,16 @@
 export const projects = [
   {
+    id: 6,
+    role: "Frontend Developer",
+    title: "SelunaTech",
+    description:
+      "A modern landing page for SelunaTech, showcasing their services and expertise in software development.",
+    image: "/images/project/selunatech.png",
+    date: "March 2026",
+    tech: ["Next.js", "Tailwind CSS"],
+    link: "selunatech-site.vercel.app",
+  },
+  {
     id: 5,
     role: "Mobile Developer",
     title: "KDMP Mobile",
